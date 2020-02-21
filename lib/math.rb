12901dfcca1c6966791d769628d6ddbf681c8372
Cindy Cai <caicindy87@gmail.com>
2020-02-21 21:19:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,17 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+puts sum
+sum
 end
 
 def subtraction(num1, num2)
-
+difference = num1 - num2
+puts 
 end
 
 def division(num1, num2)
-
+quotient = num1 / num2
+ 
 end
 
 def multiplication(num1, num2)
